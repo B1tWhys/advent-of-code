f529d9ead2cc26b199037e7d571e4a11c55d1960
skyler arnold <skyarnold1@me.com>
2024-12-28 19:02:00 -0700

    Solve day 10

---
+++ 2024/10/solution2.py
@@ -0,0 +1,51 @@
+from itertools import product
+
+if True:
+    with open("input.txt") as f:
+        text = f.read().strip()
+else:
+    text = """
+89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732
+""".strip()
+
+grid = [list(map(int, line.strip())) for line in text.split()]
+w, h = len(grid[0]), len(grid)
+
+
+def dfs(r, c, visited):
+    if (r, c) in visited:
+        return 0
+    if grid[r][c] == 9:
+        return 1
+    visited.add((r, c))
+
+    score = 0
+    cur = grid[r][c]
+    for nr, nc in [(r - 1, c), (r, c + 1), (r + 1, c), (r, c - 1)]:
+        if not (0 <= nr < h and 0 <= nc < w) or grid[nr][nc] != cur + 1:
+            continue
+        score += dfs(nr, nc, visited)
+    visited.remove((r, c))
+    return score
+
+
+def main():
+    total = 0
+    visited = set()
+    for r, c in product(range(h), range(w)):
+        if grid[r][c] == 0:
+            visited.clear()
+            score = dfs(r, c, visited)
+            total += score
+    print(f"{total=}")
+
+
+if __name__ == '__main__':
+    main()
