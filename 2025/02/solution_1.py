95485897baafb01c6c3c6aee2a0add3fc24028fd
B1tWhys <skyarnold1@me.com>
2025-12-01 21:12:13 -0800

    Add day 2 in python

---
+++ 2025/02/solution_1.py
@@ -0,0 +1,18 @@
+import re
+
+# with open("sample_input") as f:
+#     ranges = [tuple(map(int, rng.split("-"))) for rng in f.read().strip().split(",")]
+with open("input") as f:
+    ranges = [tuple(map(int, rng.split("-"))) for rng in f.read().strip().split(",")]
+
+
+def find_invalid_ids(lo, hi):
+    total = 0
+    for i in range(lo, hi + 1):
+        if re.fullmatch(r"(\d+)\1", str(i)):
+            total += i
+    print(f"{lo=} {hi=} {total=}")
+    return total
+
+
+print(sum(find_invalid_ids(*rng) for rng in ranges))
